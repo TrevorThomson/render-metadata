f5faab438755bcefb51d3fa01d9cea116e9617ab
Trevor Thomson <tgt@trevorthomson.com>
2024-04-15 12:00:34 -0400

    WIP: Refactor for alignment with design: transition database service to data access layer

--- tests/model/test_cassandra.py
+++ tests/model/test_cassandra.py
@@ -6,14 +6,14 @@ from database.model.cassandra import Cassandra
 class TestCassandra(unittest.TestCase):
 
     def test_construct(self):
-        db = Cassandra('127.0.0.1', 9042)
+        db = Cassandra(['127.0.0.1',])
         self.assertTrue(True)
     
     def test_write(self):
-        db = Cassandra('127.0.0.1', 9042)
+        db = Cassandra({'127.0.0.1',})
         data = {
             'test': 'testing'
         }
-        db.write(data)
+        db.write(keyspace='rendering',data=data)
         self.assertTrue(True)
 
